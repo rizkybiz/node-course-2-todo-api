f1d36b97ffcd2eb17a69ab3c02c7c694f6eadf2f
Justin DeFrank <justin.defrank@gmail.com>
2018-11-12 20:04:29 -0500

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,34 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB Server.');
+    }
+    console.log('Connected to MongoDB Server.');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5bea187035bf1e920740fae5')
+    // }).toArray().then((docs) => {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to find todos.', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log(`Todos count: ${count}`);
+    // }, (err) => {
+    //     console.log('Unable to find todos.', err);
+    // });
+
+    db.collection('Users').find({name: 'Justin'}).toArray().then((docs) => {
+        console.log('Users:');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to find user.', err);
+    });
+
+    //client.close();
+});
