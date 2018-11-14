268400758a5e28a6dad5bdf0a34657b8cfb9165e
Justin DeFrank <44575826+jdefrank@users.noreply.github.com>
2018-11-14 16:37:13 -0500

    updated some stuff

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -103,3 +103,33 @@ describe('GET /todos/:id', () => {
         .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('Should successfully delete a todo', (done) => {
+        var hexId = todos[0]._id.toHexString();
+
+        request(app)
+        .delete(`/todos/${hexId}`)
+        .expect(200)
+        .expect((res) => {
+            expect(res.body.todo._id).toBe(hexId);
+        }).end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+
+            Todo.findById(hexId).then((todo) => {
+                expect(todo).toNotExist();
+                done();
+            }).catch((e) => done(e));
+        });
+    });
+
+    // it('Should return 404 if todo not found', (done) => {
+
+    // });
+
+    // it('Should return 404 if objectID is invalid', (done) => {
+
+    // });
+});
